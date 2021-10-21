d02efccc940e28bab7835e95e7f02a09fcb7e5f1
Calebe-Fernandes <calebefernandesps4@gmail.com>
2021-10-21 01:38:19 -0300

    GitHub auth

--- web/src/components/loginBox/index..tsx
+++ web/src/components/loginBox/index..tsx
@@ -1,11 +1,47 @@
 import styles from './styles.module.scss';
 import{VscGithubInverted} from 'react-icons/vsc'
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+
+type AuthResponse = {
+    token:string,
+    user:{
+        id:string,
+        avatar_url:string,
+        name:string,
+        login:string
+    }
+}
 
 export function LoginBox(){
+    const signInUrl= `https://github.com/login/oauth/authorize?scope=user&client_id=0ed5cd270441d20aa49c`;
+
+    async function signIn(gitHubCode: string){
+        const response = await api.post<AuthResponse>('authenticate',{
+            code: gitHubCode
+        });
+
+        const {token, user } = response.data;
+
+        localStorage.setItem('@dowhile:token', token)
+        console.log(user);        
+    }
+    
+    useEffect(() =>{
+        const url = window.location.href;
+        const hasGitHubCode = url.includes(`?code=`);
+
+        if(hasGitHubCode){
+            const [urlWithoutCode, gitHubCode] = url.split('?code=')
+            window.history.pushState({},'',urlWithoutCode);
+            signIn(gitHubCode)
+        }
+    },[])
+
     return(
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="" className={styles.signInWithGitHub}>
+            <a href={signInUrl} className={styles.signInWithGitHub}>
                 <VscGithubInverted size = "24"/>
                 Entrar com GitHub
             </a>
--- web/src/components/messageList/index.tsx
+++ web/src/components/messageList/index.tsx
@@ -1,39 +1,46 @@
 import styles from './styles.module.scss'
 import LogoImg from '../../assets/logo.svg'
+import {api} from '../../services/api'
+import { useEffect, useState } from 'react'
+
+type Message = {
+    id:string,
+    text:string,
+    user:{
+        name:string,
+        avatar_url:string
+    }
+}
 
 export function MessageList(){
+
+    const [messages,setMessages] = useState<Message[]>([]);
+
+
+    useEffect(() =>{
+        api.get<Message[]>('messages/last3').then(response =>{
+            setMessages(response.data);
+        })
+    }, []);
+
     return(
         <div className = {styles.messageListWrapper}>
             <img src={LogoImg} alt="Do While" />
 
             <ul className = {styles.messageList}>
-                <li className = {styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/Calebe-Fernandes.png" alt="Calebe Fernandes" />
-                        </div>
-                        <span>Calebe Fernandes</span>
-                    </div>
-                </li>
-                <li className = {styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/Calebe-Fernandes.png" alt="Calebe Fernandes" />
-                        </div>
-                        <span>Calebe Fernandes</span>
-                    </div>
-                </li>
-                <li className = {styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImg}>
-                            <img src="https://github.com/Calebe-Fernandes.png" alt="Calebe Fernandes" />
-                        </div>
-                        <span>Calebe Fernandes</span>
-                    </div>
-                </li>
+                {messages.map(message => {
+                    return(
+                        <li key = {message.id} className = {styles.message}>
+                            <p className={styles.messageContent}>{message.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImg}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         
         </div>
